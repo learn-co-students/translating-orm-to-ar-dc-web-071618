3d0add65d7f6583f2a8bc9e8be04a5ce527b0497
whynotski <laurawynosky@gmail.com>
2018-07-30 09:31:59 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
-class Dog
+class Dog < ActiveRecord::Base
 end
+
+# connection =
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database =>"db/dogs.sqlite"
+#   )
+# sql =<<-SQL
+# CREATE TABLE IF NOT EXISTS dogs
+# (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT
+# )
+# SQL
+
+#ActiveRecord::Base.connection.execute(sql)
